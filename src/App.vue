4fb71d1d9921f608ac2c985d65579f62c13746dd
hirosoft40 <hirosoft40@gmail.com>
2019-05-10 23:51:17 -0500

    up to 49

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
+    {{ videos.length}}
   </div>
 </template>
 <script>
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import { API_KEY } from "./Google.js";
 
 export default {
   name: "App",
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
+  },
+  data() {
+    return { videos: [] };
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(response => {
+          this.videos = response.data.items;
+        });
     }
   }
 };
